cdaea3710a3d3f8cc23da97ade98657a7d69e777
Ali <elipoint777@gmail.com>
2022-03-14 20:17:22 +0400

    added 14 mart's task

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //Verilmiş ədədin sadə ədəd olub olmamasını print edən console app yazın.
+            //    Console.WriteLine("Ededi daxil edin:");
+            //    int num = int.Parse(Console.ReadLine());
+            //    bool prime = true;
+            //    for (int i = 2; i*i <= num ; i++)
+            //    {
+            //        if (num % i == 0)
+            //            prime = false;
+            //    }
+            //    if (prime)
+            //    {
+            //        Console.WriteLine("Sadedir");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Murekkebdir");
+            //    }
+            //}
+            #endregion
+
+            #region Task2
+            //Verilmiş ədədin neçə mərtəbəli olduğunu print edən
+            //Console.WriteLine("Ededi daxil edin:");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num < 0)
+            //{
+            //    num *= (-1);
+            //}
+            //int filan = 1;
+            //while (num > 9)
+            //{
+            //    num /= 10;
+            //    filan++;
+            //}
+            //Console.WriteLine(filan + " mertebelidir");
+            #endregion
+
+            #region Task3
+            //Verilmiş ədədin palindrom olub olmadığını print edən console app yazın.
+            //Console.WriteLine("Ededi daxil edin:");
+            //int num = int.Parse(Console.ReadLine());
+            //int example = num;
+            //int ters = 0;
+            //if (num > 9)
+            //{
+            //    while (num > 0)
+            //    {
+            //        ters = ters * 10 + num % 10;
+            //        num /= 10;
+            //    }
+            //    if (ters == example)
+            //    {
+            //        Console.WriteLine("Polindromdur");
+            //    }
+            //    else Console.WriteLine("Polindrom deyil");
+            //}
+            //else Console.WriteLine("Polindromdur");
+            #endregion
+        }
+    }
+}
